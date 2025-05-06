338338e19df2cc8ea549b363e98cab2acfdcda8e
HimanshuYadav951 <157367014+HimanshuYadav951@users.noreply.github.com>
2025-05-07 00:30:10 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import numpy as np
 import pickle
-
+import joblib
 import matplotlib.pyplot as plt
 import seaborn as sns
 from sklearn.manifold import TSNE
